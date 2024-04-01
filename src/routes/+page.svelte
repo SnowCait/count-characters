f78f29b17c40af7fc865d8ec18145cf718400f96
SnowCait <SnowCait@users.noreply.github.com>
2024-04-01 19:33:07 +0900

    Count characters

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,71 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	let text = '';
+</script>
+
+<main>
+	<h1>文字数カウント</h1>
+
+	<section>
+		<textarea bind:value={text}></textarea>
+		<div>
+			<button on:click={() => (text = '')}>リセット</button>
+		</div>
+	</section>
+
+	<section>
+		<table>
+			<tbody>
+				<tr>
+					<td>文字数（スペース込み）</td>
+					<td><input type="text" value={text.replace('\n', '').length} /></td>
+				</tr>
+				<tr>
+					<td>文字数（スペース無視）</td>
+					<td><input type="text" value={text.replace(/\s|　/g, '').length} /></td>
+				</tr>
+				<tr>
+					<td>行数</td>
+					<td><input type="text" value={(text.match(/\n/g)?.length ?? 0) + 1} /></td>
+				</tr>
+				<tr>
+					<td>段落数</td>
+					<td>
+						<input
+							type="text"
+							value={(text.match(/\n(?:　|\s+|「|『|＜|《|〈|≪|（|“|‘|\(|\"|\')./g)?.length ?? 0) +
+								1}
+						/>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</section>
+</main>
+
+<style>
+	main {
+		text-align: center;
+	}
+
+	section {
+		margin: 2rem auto;
+	}
+
+	textarea {
+		width: 100%;
+		max-width: 800px;
+		height: 20rem;
+	}
+
+	table {
+		margin: 0 auto;
+	}
+
+	td {
+		text-align: left;
+	}
+
+	input {
+		width: 5rem;
+	}
+</style>
