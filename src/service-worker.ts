21c0c3f366c8d7427184bef7393125605d2fb446
SnowCait <SnowCait@users.noreply.github.com>
2024-04-02 00:15:11 +0900

    Cache

---
+++ src/service-worker.ts
@@ -0,0 +1,84 @@
+/// <reference types="@sveltejs/kit" />
+/// <reference no-default-lib="true"/>
+/// <reference lib="esnext" />
+/// <reference lib="webworker" />
+
+import { build, files, version } from '$service-worker';
+
+const sw = self as unknown as ServiceWorkerGlobalScope;
+
+const CACHE = `cache-${version}`;
+const ASSETS = [
+	...build, // the app itself
+	...files // everything in `static`
+];
+
+sw.addEventListener('install', (event) => {
+	console.debug('[service worker install]');
+
+	async function addFilesToCache() {
+		const cache = await caches.open(CACHE);
+		await cache.addAll(ASSETS);
+	}
+
+	event.waitUntil(addFilesToCache());
+});
+
+sw.addEventListener('activate', (event) => {
+	console.debug('[service worker activate]');
+
+	async function deleteOldCaches() {
+		for (const key of await caches.keys()) {
+			if (key !== CACHE) {
+				await caches.delete(key);
+			}
+		}
+	}
+
+	event.waitUntil(deleteOldCaches());
+});
+
+sw.addEventListener('fetch', (event) => {
+	console.debug('[service worker fetch]', event.request);
+
+	if (!event.request.url.startsWith('http') || event.request.method !== 'GET') {
+		return;
+	}
+
+	async function respond() {
+		const url = new URL(event.request.url);
+		const cache = await caches.open(CACHE);
+
+		if (ASSETS.includes(url.pathname)) {
+			const response = await cache.match(url.pathname);
+
+			if (response) {
+				return response;
+			}
+		}
+
+		try {
+			const response = await fetch(event.request);
+
+			if (!(response instanceof Response)) {
+				throw new Error('invalid response from fetch');
+			}
+
+			if (response.status === 200) {
+				cache.put(event.request, response.clone());
+			}
+
+			return response;
+		} catch (error) {
+			const response = await cache.match(event.request);
+
+			if (response) {
+				return response;
+			}
+
+			throw error;
+		}
+	}
+
+	event.respondWith(respond());
+});
